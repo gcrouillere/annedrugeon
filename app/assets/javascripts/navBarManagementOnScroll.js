a1326a7b3429b02e05457f7932de51029bd8df14
guillaume crouillere <gcrouillere@hotmail.fr>
2018-10-03 14:36:02 +0200

    contact visual changes

--- app/assets/javascripts/navBarManagementOnScroll.js
+++ app/assets/javascripts/navBarManagementOnScroll.js
@@ -15,8 +15,8 @@ $(document).ready(function() {
         borderLeft: 'none'
       });
       $('.navbar-laptop-lighttheme').removeClass('navbar-on-scroll');
-      if ($(window).innerWidth() > 767) {$('.content').css("padding-top", '183px');}
-      if ($('.alert.alert-dismissible')) {$('.alert.alert-dismissible').css("top", '183px');}
+      if ($(window).innerWidth() > 767) {$('.content').css("padding-top", '203px');}
+      if ($('.alert.alert-dismissible')) {$('.alert.alert-dismissible').css("top", '203px');}
     }
   }
 
