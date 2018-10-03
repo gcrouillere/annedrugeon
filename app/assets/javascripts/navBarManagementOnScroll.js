21450ae932edaaadb7b933adff09056d80fd0205
guillaume crouillere <gcrouillere@hotmail.fr>
2018-10-03 15:16:21 +0200

    increased content padding top

--- app/assets/javascripts/navBarManagementOnScroll.js
+++ app/assets/javascripts/navBarManagementOnScroll.js
@@ -7,8 +7,8 @@ $(document).ready(function() {
         borderLeft: '1px solid black'
       });
       $('.navbar-laptop-lighttheme').addClass('navbar-on-scroll');
-      if ($(window).innerWidth() > 767) {$('.content').css("padding-top", '110px');}
-      if ($('.alert.alert-dismissible')) {$('.alert.alert-dismissible').css("top", '70px');}
+      if ($(window).innerWidth() > 767) {$('.content').css("padding-top", '113px');}
+      if ($('.alert.alert-dismissible')) {$('.alert.alert-dismissible').css("top", '113px');}
     } else {
       $('.centered-logo').css({display: 'block'});
       $('.third-link').css({
