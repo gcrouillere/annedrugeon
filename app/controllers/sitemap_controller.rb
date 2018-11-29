e16e5cff2b7dc65b9c52e0d560d66d826210c095
guillaume crouillere <gcrouillere@hotmail.fr>
2018-11-29 12:19:05 +0100

    add sitemap

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -3,6 +3,7 @@ class SitemapController < ApplicationController
   skip_before_action :authenticate_user!, only: [:sitemap]
 
   def sitemap
-    @ceramiques = Ceramique.all
+    @ceramiques = Ceramique.where(active: true)
+    @collections = Collection.all
   end
 end
