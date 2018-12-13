90afe1ed67e0a20dfd9d1da36b5c2224d4f39e46
guillaume crouillere <gcrouillere@hotmail.fr>
2018-12-13 14:47:06 +0100

    changed sitemap

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -5,5 +5,7 @@ class SitemapController < ApplicationController
   def sitemap
     @ceramiques = Ceramique.where(active: true)
     @collections = Collection.all
+    @sellpoint = Article.where("name LIKE ? OR name LIKE ?", "sellpoint", "ephemeral").order(updated_at: :desc).first
+    @info_article = Article.where("name LIKE ?", "%info%").order(updated_at: :desc).first
   end
 end
